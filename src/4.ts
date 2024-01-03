017478b290037fa94046d4416c42582e3b20164d
okaisym <okaisim@gmail.com>
2024-01-04 01:13:34 +0200

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+    constructor() {
+      this.signature = Math.random();
+    }
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+    constructor(key: Key) {
+      this.key = key;
+    }
+    comeIn(person: Person) {
+      if (this.door) {
+        this.tenants.push(person);
+      }
+    }
+    abstract openDoor(key: Key | undefined): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (this.key.getSignature() === key.getSignature()) {
+        this.door = true;
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
